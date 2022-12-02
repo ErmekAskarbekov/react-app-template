9f401c38241c30f7b3714d931dd19cce557f23e3
ermek <ermaaskarbekov@gmail.com>
2022-12-02 23:06:57 +0600

    Создал компонент header

---
+++ src/components/UI/Header/index.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+import Container from "components/UI/Container";
+import {ILink} from 'utils/types/index';
+
+interface Props {
+    links: ILink[];
+}
+
+const Header: FC<Props> = ({links}) => {
+    return (
+        <header
+            className='flex justify-between items-center h-[60px] bg-white sticky top-0 shadow-2xl'>
+            <Container>
+                <ul className="flex justify-between ">
+                    {links.map((link) => (
+                        <li className='list-none mr-2 text-black hover:text-gray-600 transition-all font-s text-s'
+                            key={`link/${link.url}`}>
+                            <a href={link.url}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
