64fdda505ec6c6a89ad71ad3e3e54487864f37d7
ermek <ermaaskarbekov@gmail.com>
2022-12-03 03:18:13 +0600

    Обновил типы

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -2,3 +2,10 @@ export interface ILink {
     url: string;
     title: string;
 }
+
+export interface IPost {
+    title: string;
+    description: string;
+    id: string;
+    date?: string | number;
+}
