9dba2c7970e11ad7d7b397798a9dc09c7cd8a1ce
ermek <ermaaskarbekov@gmail.com>
2022-12-03 00:19:26 +0600

    Добавил ссылки в список навигаций

--- src/utils/list/index.ts
+++ src/utils/list/index.ts
@@ -2,4 +2,7 @@ import {ILink} from "utils/types";
 
 export const linksList: ILink[] = [
     {title: 'Home', url: '/'},
+    {title: 'Add', url: '/add'},
+    {title: 'About', url: '/about'},
+    {title: 'Contacts', url: '/contacts'},
 ]
