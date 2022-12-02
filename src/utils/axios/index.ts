f65048c50ee2e6743945372a95ae9dc4fab50c86
ermek <ermaaskarbekov@gmail.com>
2022-12-03 01:25:18 +0600

    Настроил Axios

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const RestCounty = axios.create({baseURL: 'https://restcountries.com/'});
+const FireBase = axios.create({baseURL: 'https://my-blog-47c65-default-rtdb.asia-southeast1.firebasedatabase.app/', withCredentials: false});
+
+export default FireBase;
