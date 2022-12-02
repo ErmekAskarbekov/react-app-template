a3b918f3022a577d395277da7a73c942c5c7e179
ermek <ermaaskarbekov@gmail.com>
2022-12-03 03:16:06 +0600

    Сверстал страницу моих контактов

---
+++ src/screens/contacts.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from "components/UI/Container";
+
+const Contacts = () => {
+    return (
+        <Container>
+            <h1 className='text-4xl mb4'>Contacts</h1>
+            <p><b>Tel: </b>+ (996) 771 58 91 58</p>
+            <p><b>Email: </b> Ermaaskarbekov@gmail.com</p>
+        </Container>
+    );
+};
+
+export default Contacts;
