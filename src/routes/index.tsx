6f927aed3b9c524e531bcddca18292befc209ce8
ermek <ermaaskarbekov@gmail.com>
2022-12-03 00:18:02 +0600

    Добавил роуты

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import React, {FC} from 'react';
 import {Route, Routes} from "react-router-dom";
 import Home from "screens/home";
+import Add from "../screens/add";
+import About from "../screens/about";
+import Contacts from "../screens/contacts";
 
 const RoutesList: FC = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/add' element={<Add/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contacts' element={<Contacts/>}/>
         </Routes>
     );
 };
