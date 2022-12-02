74902570db9669df9a517ebde0b0378b40601a0a
ermek <ermaaskarbekov@gmail.com>
2022-12-03 03:18:05 +0600

    Обновил роуты

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,8 @@ import Home from "screens/home";
 import Add from "screens/add";
 import About from "screens/about";
 import Contacts from "screens/contacts";
+import Post from "screens/post";
+import Edit from "screens/edit";
 
 const RoutesList: FC = () => {
     return (
@@ -12,6 +14,8 @@ const RoutesList: FC = () => {
             <Route path='/new-post' element={<Add/>}/>
             <Route path='/about' element={<About/>}/>
             <Route path='/contacts' element={<Contacts/>}/>
+            <Route path='/post/:id' element={<Post/>}/>
+            <Route path='/post/:id/edit' element={<Edit/>}/>
         </Routes>
     );
 };
