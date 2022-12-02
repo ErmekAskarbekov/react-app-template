e7ca7d69cf2c788bfb8e317c70d37ca1bd32300b
ermek <ermaaskarbekov@gmail.com>
2022-12-03 03:15:54 +0600

    Сверстал страницу обо мне

---
+++ src/screens/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from "components/UI/Container";
+
+
+const About = () => {
+    return (
+        <Container>
+            <h1 className='text-4xl mb4'>About me</h1>
+            <p><b>Name: </b> Ermek Askarbekov</p>
+            <p><b>Age: </b> 26</p>
+            <p>Hello world! I'm JavaScript Engineer without experience! I'm looking for job (trainee).</p>
+        </Container>
+    );
+};
+
+export default About;
