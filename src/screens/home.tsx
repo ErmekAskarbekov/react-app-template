4620a757fe515eac450f080878531dac21e49c3a
ermek <ermaaskarbekov@gmail.com>
2022-12-03 03:18:29 +0600

    Обновил домашнюю страницу

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,37 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import PostCard from "components/PostCard";
+import Container from "components/UI/Container";
+import FireBase from "utils/axios";
+import {IPost} from "utils/types";
 
 const Home: FC = () => {
+    const [posts, setPosts] = useState<IPost[]>([]);
+
+    const fetchData = async () => {
+        try {
+            const {data} = await FireBase.get('posts.json');
+
+            const array = Object.keys(data).map((post) => ({id: post, ...data[post]}));
+            console.log(data);
+            console.log(Object.keys(data));
+            console.log(array)
+
+            setPosts(array);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <Container>
+            {posts.length > 0 ? posts.map((post: IPost) => (
+                <PostCard key={post.id} {...post}/>
+            )) : <p>No posts</p>}
+        </Container>
     );
 };
 
