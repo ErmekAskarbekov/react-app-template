094637992bfd9f0a3d149451e572a98c855dce76
ermek <ermaaskarbekov@gmail.com>
2022-12-03 01:25:47 +0600

    Обновил список ссылок

--- src/utils/list/index.ts
+++ src/utils/list/index.ts
@@ -2,7 +2,7 @@ import {ILink} from "utils/types";
 
 export const linksList: ILink[] = [
     {title: 'Home', url: '/'},
-    {title: 'Add', url: '/add'},
+    {title: 'Add', url: '/new-post'},
     {title: 'About', url: '/about'},
     {title: 'Contacts', url: '/contacts'},
 ]
